48dcc9de6f6f3e377d0c09bb3140540217432f53
Vitor Arakaki <sarahguerra2006@gmail.com>
2026-03-08 19:45:00 -0300

    imgs

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Image from 'next/image';
+import monkeyImg from '/monkeyimg.jpeg'
+import monkeyGif from '/monkey.gif'
 
 function Home() {
   return <>
-  <h1>Em construção</h1>
+  <h1>Em construção =]</h1>
+  <Image src={monkeyImg} alt="monkey img"/>
+  <Image src={monkeyGif} alt="monkey gif"/>
   </>;
 }
 
