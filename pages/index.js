55d1d949616b3d18a0a83db6c62d1adc6bd5268c
Vitor Arakaki <sarahguerra2006@gmail.com>
2026-03-08 19:20:13 -0300

    deploy de pagina em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Image from 'next/image';
+import monkeyImg from '../pages/monkeyimg.jpeg'
+import monkeyGif from '../pages/monkey.gif'
+
 function Home() {
-  return <h1>Testing deploy</h1>;
+  return <>
+  <h1>Em construção</h1>
+  <Image src={monkeyImg} alt="monkey img"/>
+  <Image src={monkeyGif} alt="monkey gif"/>
+  </>;
 }
 
 export default Home;
