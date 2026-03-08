7a5e48a2a1faf43775c24f1ec2b905207835a630
Helcony <sarahguerra2006@gmail.com>
2026-03-08 19:39:52 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import Image from 'next/image';
-import monkeyImg from '/monkeyimg.jpeg'
-import monkeyGif from '/monkey.gif'
 
 function Home() {
   return <>
   <h1>Em construção</h1>
-  <Image src={monkeyImg} alt="monkey img"/>
-  <Image src={monkeyGif} alt="monkey gif"/>
   </>;
 }
 
