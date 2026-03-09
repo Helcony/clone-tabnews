7c67ced6e08fff47ef7f5bda9b855a6013e61e22
Vitor Arakaki <sarahguerra2006@gmail.com>
2026-03-09 18:15:18 -0300

    teste automatizado de mintir

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-import Image from 'next/image';
-import monkeyImg from '/public/monkeyimg.jpeg'
-import monkeyGif from '/public/monkey.gif'
+import Image from "next/image";
+import monkeyImg from "/public/monkeyimg.jpeg";
+import monkeyGif from "/public/monkey.gif";
 
 function Home() {
-  return <>
-  <h1>Em construção =]</h1>
-  <Image src={monkeyImg} alt="monkey img"/>
-  <Image src={monkeyGif} alt="monkey gif"/>
-  </>;
+  return (
+    <>
+      <h1>Em construção =]</h1>
+      <Image src={monkeyImg} alt="monkey img" />
+      <Image src={monkeyGif} alt="monkey gif" />
+    </>
+  );
 }
 
 export default Home;
