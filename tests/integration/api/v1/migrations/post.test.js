41f62d16fe0e960fc3b3f10845ab97af5b0f869a
Helcony <helconycoding@gmail.com>
2026-06-27 22:48:24 +0900

    make test scripts more robust and allow jest to access .env.dev

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,11 +1,28 @@
+import database from "infra/database"
+
+beforeAll(cleanDatabase)
+
+async function cleanDatabase() {
+    await database.query("drop schema public cascade; create schema public")
+}
+
 test("POST to /api/v1/migrations should return 200", async () => {
     const res = await fetch("http://localhost:3000/api/v1/migrations", {
         method: 'POST',
     })
-    expect(res.status).toBe(200)
+    expect(res.status).toBe(201)
 
     const responseBody = await res.json()
-    console.log(responseBody)
 
-    expect(Array.isArray(responseBody)).toBe(true)
+    expect(responseBody.length).toBeGreaterThan(0)
+
+    const res1 = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: 'POST',
+    })
+    expect(res1.status).toBe(200)
+
+    const responseBody1 = await res1.json()
+
+    expect(responseBody1.length).toBe(0)
+
 })
