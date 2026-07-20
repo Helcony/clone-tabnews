2e30674391350b44960fbfeb5339b85a2e5c23d4
Helcony <helconycoding@gmail.com>
2026-07-20 22:06:33 +0900

    style: runs prettier, fixes unescaped character from eslint

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,56 +1,61 @@
-import useSWR from "swr"
+import useSWR from "swr";
 
 async function fetchAPI(key) {
-  const res = await fetch (key)
-  const resBody = await res.json()
-  return resBody
+  const res = await fetch(key);
+  const resBody = await res.json();
+  return resBody;
 }
 
 export default function StatusPage() {
   return (
-  <>
-    <h1>Status page</h1>
-    <UpdatedAt />
-    <DatabaseStatus />
-  </>
-  )
+    <>
+      <h1>Status page</h1>
+      <UpdatedAt />
+      <DatabaseStatus />
+    </>
+  );
 }
 
 function UpdatedAt() {
   const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 2000,
-  })
+  });
 
-  let UpdatedAtText = "Loading..."
+  let UpdatedAtText = "Loading...";
 
-  if(!isLoading && data) {
-    UpdatedAtText = new Date(data.updated_at).toLocaleString("pt-BR")
+  if (!isLoading && data) {
+    UpdatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
   }
 
-  return <div>Última atualização: {UpdatedAtText}</div>
+  return <div>Última atualização: {UpdatedAtText}</div>;
 }
 
 function DatabaseStatus() {
   const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 2000,
-  })
+  });
 
-  let databaseText = "Loading..."
+  let databaseText = "Loading...";
 
-  if(!isLoading && data) {
-    const databaseJsonPath = data.dependencies.database
-    databaseText = (<>
-      <div>Version: {databaseJsonPath.version}</div>
-      <div>Connection limit: {databaseJsonPath.max_connections}</div>
-      <div>Current connections: {databaseJsonPath.current_connections}</div>
-      <div>Current connections on database (idk how to get the params yet, so this won't work): {databaseJsonPath.current_db_connections}</div>
-    </>)
+  if (!isLoading && data) {
+    const databaseJsonPath = data.dependencies.database;
+    databaseText = (
+      <>
+        <div>Version: {databaseJsonPath.version}</div>
+        <div>Connection limit: {databaseJsonPath.max_connections}</div>
+        <div>Current connections: {databaseJsonPath.current_connections}</div>
+        <div>
+          Current connections on database (idk how to get the params yet, so
+          this wont work): {databaseJsonPath.current_db_connections}
+        </div>
+      </>
+    );
   }
 
-  return <>
-  <h2>Database</h2>
-  <div>{databaseText}</div>
-  </>
-
-  
+  return (
+    <>
+      <h2>Database</h2>
+      <div>{databaseText}</div>
+    </>
+  );
 }
